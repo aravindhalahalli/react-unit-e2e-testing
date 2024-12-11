921af6606895449a8a0730db068c0e92fb12375e
Aravind Kumar <arakumar@griddynamics.com>
2024-12-11 14:44:03 +0530

    testing an component mocking an axios with spyOn

--- src/tag/Tag.test.jsx
+++ src/tag/Tag.test.jsx
@@ -1,25 +1,47 @@
 import { render, screen } from "@testing-library/react";
-import { afterAll, afterEach, beforeAll, describe, expect, it } from "vitest";
+import { afterAll, afterEach, beforeAll, describe, expect, it, vi } from "vitest";
 import Tag from "./Tag";
 import { setupServer } from "msw/node";
 import { http, HttpResponse } from "msw";
+import axios from "axios";
 
 describe("Tag component", () => {
   const server = setupServer(
     http.get("http://localhost:3004/tags", () => {
-      return HttpResponse.json([{ id: 1, name: "React" }]);
+      return HttpResponse.json([
+        { id: 1, name: "React" },
+        { id: 2, name: "Typescript" },
+      ]);
     })
   );
 
   beforeAll(() => server.listen());
   afterAll(() => server.close());
   afterEach(() => server.resetHandlers());
-  it("renders the tags", async() => {
+  it("renders the tags", async () => {
     render(<Tag />);
     // use an findAllByTestId when using an async and await
-    // const tags = await screen.getAllByTestId("tag");  
+    // const tags = await screen.getAllByTestId("tag");
     const tags = await screen.findAllByTestId("tag");
-    screen.debug()
-    expect(tags).toHaveLength(1);
+    screen.debug();
+    expect(tags).toHaveLength(2);
+    expect(tags[0]).toHaveTextContent("React");
+  });
+
+  it("render with axios mock", async () => {
+    const mockResponse = {
+      data: [
+        { id: 1, name: "Vue Js" },
+        { id: 2, name: "Typescript" },
+      ],
+    };
+    vi.spyOn(axios,'get').mockResolvedValue(mockResponse);
+    render(<Tag />);
+    // use an findAllByTestId when using an async and await
+    // const tags = await screen.getAllByTestId("tag");
+    const tags = await screen.findAllByTestId("tag");
+    screen.debug();
+    expect(tags).toHaveLength(2);
+    expect(tags[0]).toHaveTextContent("Vue Js");
   });
 });
